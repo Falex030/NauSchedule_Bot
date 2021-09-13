9cff1551eadefc5094893a82c92093e60276539c
Ilya <grynyshyn080@mail.com>
2021-09-13 22:04:22 +0300

    Redis

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -7,7 +7,9 @@ from loader import dp
 @dp.message_handler(CommandHelp())
 async def bot_help(message: types.Message):
     text = ("Список команд: ",
-            "/start - Начать диалог",
-            "/help - Получить справку")
+            "/start - Для початку спілкування з ботом",
+            "/help - Допомога по командам бота",
+            '/about - Інформація про бота')
+
     
     await message.answer("\n".join(text))
--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -4,7 +4,8 @@ from aiogram import types
 async def set_default_commands(dp):
     await dp.bot.set_my_commands(
         [
-            types.BotCommand("start", "Запустить бота"),
-            types.BotCommand("help", "Вывести справку"),
+            types.BotCommand("start", "Запустити бота"),
+            types.BotCommand("help", "Вивести справку"),
+            types.BotCommand("about",'Вивести інформацію про бота')
         ]
     )
