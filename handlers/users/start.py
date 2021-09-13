35fb6155d3a2441a1358a330c22f5e4c42508a9a
Ilya <grynyshyn080@mail.com>
2021-09-13 22:40:09 +0300

    Add welcome_text

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,6 +1,7 @@
 import asyncpg
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart, Text
+from aiogram.utils.emoji import emojize
 
 from keyboards.default import who_are_you
 from loader import dp
@@ -12,12 +13,24 @@ from states.botStates import StatesOfBot
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
     name = message.from_user.full_name
+    welcome_text = (
+        f"Привіт! {emojize(':wave:')}"
+        "\nЯ — твоя права рука під час навчального року,"
+        " адже в мене ти завжди можеш дізнатись, які в тебе пари протягом тижня"
+        f"{emojize(':smiling_imp:')}"
+        f"\n\nПотрібна допомога з командами? Просто відправ мені /help"
+        f"\n\nХочеш більше дізнатися про бота?"
+        f" Знаєш як його покращити?"
+        f" Щось пішло не так?"
+        f"\nВсю потрібну інформацію ти знайдеш натиснувши /about"
+
+        f"\n\nДля початку, скажи мені хто ти{emojize(':smirk_cat:')}:"
+    )
     # try:
     #     await commands.add_user(id=message.from_user.id,name=name)
     # except asyncpg.exceptions.UniqueViolationError:
     #     pass
 
-    await message.answer(f"Привет, {message.from_user.full_name}!\n"
-                         "Ким ти явлаєшся?",
+    await message.answer(welcome_text,
                          reply_markup= who_are_you)
     # await StatesOfBot.start_state.set()
