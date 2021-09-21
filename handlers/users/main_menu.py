7605d03dc72c1663fcc17265d7d9e0c24da3ad6c
Falex030 <grynyshyn080@gmail.com>
2021-09-21 09:42:26 +0300

    Add fuctionality schedule time

--- handlers/users/main_menu.py
+++ handlers/users/main_menu.py
@@ -1,5 +1,13 @@
 from aiogram.types import Message
-
+from aiogram.dispatcher.filters.builtin import Text
+from aiogram.types import Message, ReplyKeyboardRemove
+from keyboards.default import main_menu
+from loader import dp, bot
+from aiogram.dispatcher import FSMContext
+from states.botStates import StatesOfBot
+from keyboards.default import main_menu
+from aiogram.types import Message
+from aiogram.utils.emoji import emojize
 from loader import dp
 
 
@@ -15,10 +23,19 @@ async def schedule_tomorrow(message: Message):
     await message.answer('Розклад на завтра')
 
 
-@dp.message_handler(text="Розклад")
-async def schedule(message: Message):
-    # функціонал виводу інформації про завтрішній  розклад
-    await message.answer('Розклад ')
+
+@dp.message_handlers(text='Розклад')
+async def serch_schedue(message : Message):
+    text = (
+        f'{emojize(":bell:")} 1 пара \n {emojize(":alarm_clock:")} 8:00 - 9:35 \n' 
+        f'{emojize(":bell:")} 2 пара \n {emojize(":alarm_clock:")} 9:50-11:25 \n'
+        f'{emojize(":bell:")} 3 пара \n {emojize(":alarm_clock:")} 11:40 - 13:15 \n'
+        f'{emojize(":bell:")} 4 пара \n {emojize(":alarm_clock:")} 13:30-15:05 \n'
+        f'{emojize(":bell:")} 5 пара \n {emojize(":alarm_clock:")} 15:20-16:55 \n'
+        f'{emojize(":bell:")} 6 пара \n {emojize(":alarm_clock:")} 17:10-18:45 \n'
+        f'{emojize(":bell:")} 7 пара \n {emojize(":alarm_clock:")} 19:00-20:35')
+    await message.answer(text)
+
 
 
 @dp.message_handler(text="Розклад дзвінків")
