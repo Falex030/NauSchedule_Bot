5ea74b8cafc064b63a57ac9cbfd15b4be6641273
Ilya <grynyshyn080@mail.com>
2021-09-22 13:34:59 +0300

    edit settings.py

--- handlers/users/settings.py
+++ handlers/users/settings.py
@@ -1,9 +1,7 @@
 from aiogram.types import Message
-from handlers.users.about_bot import bot_about
+
 from keyboards.default.settings_key import settings
 from loader import dp
-from keyboards.default.about_bot_key import keyboard_about
-from handlers.users.main_menu import main_page
 from utils.misc import rate_limit
 
 
